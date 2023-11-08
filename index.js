d9f82e4df803fc11fc236a91ce7cf176ef1eadb7
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-08 14:30:42 -0500

    const and console

---
+++ index.js
@@ -0,0 +1,11 @@
+//console.log("hello");
+//const froyoFlavors = ["vanilla", "strawberry", "coffee"];
+//console.log(froyoFlavors);
+
+const froyoFlavors = ["vanilla", "strawberry", "coffee" ];
+console.log(froyoFlavors);
+
+froyoFlavors [0] = 3;
+froyoFlavors [1] = 1;
+froyoFlavors [2] = 2;
+console.log(froyoFlavors)
